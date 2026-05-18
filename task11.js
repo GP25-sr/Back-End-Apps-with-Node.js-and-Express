3abd20eb16d2443cdccac9ef15926b5a8da1a8a6
Geeta pandit <geetapandit004@gmail.com>
2026-05-19 00:05:59 +0530

    Create task11.js

---
+++ task11.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+axios.get('http://localhost:5000/isbn/1')
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
