21c6899be8c60e2a85821627b3b6782946ab5e23
Geeta pandit <geetapandit004@gmail.com>
2026-05-19 07:36:42 +0530

    Delete task11.js

--- task11.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://localhost:5000/isbn/1')
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
